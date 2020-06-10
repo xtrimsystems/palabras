4851e85348a4694b54919ae5f2f446e415f16367
xtrimsystems <xtrimsystems@gmail.com>
2020-06-10 23:10:02 +0200

    Added two new categories, "home things" and "parts of the body". Start removing family category.

--- src/Categories.ts
+++ src/Categories.ts
@@ -19,7 +19,17 @@ export const Categories: I18nCategories[] = [
 				id: -3,
 				name: 'Chuches',
 				imageBase64: 'images/candy.jpg'
-			}
+			},
+			{
+				id: -4,
+				name: 'Cosas de casa',
+				imageBase64: 'images/house.jpeg'
+			},
+			{
+				id: -5,
+				name: 'Partes del cuerpo',
+				imageBase64: 'images/body.png'
+			},
 		],
 		stages: [
 			{
@@ -122,55 +132,55 @@ export const Categories: I18nCategories[] = [
 				id: -1,
 				categoryId: -2,
 				word: 'abuela',
-				imageBase64: 'images/words/abuela.png',
+				imageBase64: 'images/family/abuela.png',
 			},
 			{
 				id: -1,
 				categoryId: -2,
 				word: 'abuelo',
-				imageBase64: 'images/words/abuelo.png',
+				imageBase64: 'images/family/abuelo.png',
 			},
 			{
 				id: -1,
 				categoryId: -2,
 				word: 'andres',
-				imageBase64: 'images/words/andres.png',
+				imageBase64: 'images/family/andres.png',
 			},
 			{
 				id: -1,
 				categoryId: -2,
 				word: 'dani',
-				imageBase64: 'images/words/dani.png',
+				imageBase64: 'images/family/dani.png',
 			},
 			{
 				id: -1,
 				categoryId: -2,
 				word: 'mama',
-				imageBase64: 'images/words/mama.png',
+				imageBase64: 'images/family/mama.png',
 			},
 			{
 				id: -1,
 				categoryId: -2,
 				word: 'martina',
-				imageBase64: 'images/words/martina.png',
+				imageBase64: 'images/family/martina.png',
 			},
 			{
 				id: -1,
 				categoryId: -2,
 				word: 'papa',
-				imageBase64: 'images/words/papa.png',
+				imageBase64: 'images/family/papa.png',
 			},
 			{
 				id: -1,
 				categoryId: -2,
 				word: 'sara',
-				imageBase64: 'images/words/sara.png',
+				imageBase64: 'images/family/sara.png',
 			},
 			{
 				id: -1,
 				categoryId: -2,
 				word: 'sophie',
-				imageBase64: 'images/words/sophie.png',
+				imageBase64: 'images/family/sophie.png',
 			},
 			{
 				id: -1,
@@ -183,7 +193,265 @@ export const Categories: I18nCategories[] = [
 				categoryId: -3,
 				word: 'chupachups',
 				imageBase64: 'images/words/chupa-chups.jpg'
-			}
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'cama',
+				imageBase64: 'images/words/bed.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'cuento',
+				imageBase64: 'images/words/book.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'botella',
+				imageBase64: 'images/words/bottle.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'silla',
+				imageBase64: 'images/words/chair.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'cuco',
+				imageBase64: 'images/words/cuco.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'taza',
+				imageBase64: 'images/words/cup.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'puerta',
+				imageBase64: 'images/words/door.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'biberon',
+				imageBase64: 'images/words/feeding-bottle.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'tenedor',
+				imageBase64: 'images/words/fork.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'jardin',
+				imageBase64: 'images/words/garden.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'vaso',
+				imageBase64: 'images/words/glass.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'gafas',
+				imageBase64: 'images/words/glasses.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'guantes',
+				imageBase64: 'images/words/gloves.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'peine',
+				imageBase64: 'images/words/hairbrush.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'mano',
+				imageBase64: 'images/words/hand.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'sombrero',
+				imageBase64: 'images/words/hat.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'cocina',
+				imageBase64: 'images/words/kitchen.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'cuchillo',
+				imageBase64: 'images/words/knife.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'sarten',
+				imageBase64: 'images/words/pan.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'lapizero',
+				imageBase64: 'images/words/pencil.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'piano',
+				imageBase64: 'images/words/piano.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'planta',
+				imageBase64: 'images/words/plant.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'plato',
+				imageBase64: 'images/words/plate.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'cazo',
+				imageBase64: 'images/words/saucepan.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'tijeras',
+				imageBase64: 'images/words/scissors.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'cucharita',
+				imageBase64: 'images/words/small-spoon.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'escaleras',
+				imageBase64: 'images/words/stairs.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'mesa',
+				imageBase64: 'images/words/table.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'cepillo',
+				imageBase64: 'images/words/toothbrush.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'pasta',
+				imageBase64: 'images/words/toothpaste.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'cocinita',
+				imageBase64: 'images/words/toy-kitchen.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'juguetes',
+				imageBase64: 'images/words/toys.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'arbol',
+				imageBase64: 'images/words/tree.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'paraguas',
+				imageBase64: 'images/words/umbrella.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -4,
+				word: 'ventana',
+				imageBase64: 'images/words/window.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -5,
+				word: 'brazo',
+				imageBase64: 'images/words/arm.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -5,
+				word: 'oreja',
+				imageBase64: 'images/words/ear.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -5,
+				word: 'ojo',
+				imageBase64: 'images/words/eye.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -5,
+				word: 'pie',
+				imageBase64: 'images/words/foot.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -5,
+				word: 'mano',
+				imageBase64: 'images/words/hand.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -5,
+				word: 'piernas',
+				imageBase64: 'images/words/legs.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -5,
+				word: 'nariz',
+				imageBase64: 'images/words/nose.jpg'
+			},
+			{
+				id: -1,
+				categoryId: -5,
+				word: 'dientes',
+				imageBase64: 'images/words/teeth.jpg'
+			},
 		]
 	},
 	{
